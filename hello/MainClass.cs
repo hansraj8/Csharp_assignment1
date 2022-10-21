b1d748246b257b77554fc91eb37cfbc9e0e94b8b
Abhishek Kumar <hansrajsharma8252@gmail.com>
2022-10-21 13:33:32 +0530

    Add project files.

---
+++ hello/MainClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* namespace assignment2
+{ 
+class MainClass
+    {
+  
+        public static void Main(string[] args)
+        {
+            for (int input = 1; input <= 10; input++)
+            {
+                Console.WriteLine(input);
+            Console.ReadKey();
+            }
+        }
+    }
+}
+
+*/
